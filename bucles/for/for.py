7d3f2960c09f69ab29e11429dfffe94624436d09
jkest07 <ejuan5427@gmail.com>
2025-08-15 21:33:14 -0500

    bucles

---
+++ bucles/for/for.py
@@ -0,0 +1,10 @@
+secuencia = [1, 2, 3, 4, 5]
+diccionario = {'a': 1, 'b': 2, 'c': 3}
+
+for numero in secuencia:
+    print(numero)
+
+for clave, valor in diccionario.items():
+    print(f'Clave: {clave}, Valor: {valor}')
+
+
---
+++ bucles/while/while.py
@@ -0,0 +1,4 @@
+c = 1
+while c <= 20:
+    print(c)
+    c += 1
